b199f117d34639d3b006f28addf91a47487b0d74
Zhandos <saginov-2001@mail.ru>
2018-07-17 16:48:03 +0300

    fdsfdsfdsf

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<title>Landing</title>
 </head>
 <body>
-	<div class="container">
-		
+	<div class="page-wrap">
+		<div class="header">
+			<img src="images/bg-head.png" alt="header" class="header-image">
+			<div class="button">Заказать</div>
+		</div>
+		<div class="container">
+			
+		</div>
 	</div>
+		
 </body>
 </html>
