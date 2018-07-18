3cea062401ad855736b05f8e963b9cbc4409f25d
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-07-18 17:11:02 +0500

    gg

--- index.php
+++ index.php
@@ -8,11 +8,29 @@
 <body>
 	<div class="page-wrap">
 		<div class="header">
-			<img src="images/bg-head.png" alt="header" class="header-image">
-			<div class="button">Заказать</div>
+			<img src="images/web.jpg" alt="header" class="bg-image">
+			<div class="title">
+				<div class="hook">
+					В 90% малого бизнеса <br>
+ключевая проблема - нехватка клиентов
+				</div>
+				<div class="poloska">
+					<hr>
+				</div> 
+				<div class="idea">ПОЛУЧАЙТЕ ДО 500 НОВЫХ ЗАКАЗОВ <br>
+КАЖДЫЙ МЕСЯЦ - НЕ ЗАТРАЧИВАЯ УСИЛИЙ</div>
+<br>
+				<div class="main-idea">Ваш продающий сайт с уникальным дизайном 
+					<br>
+в топе Яндекс и Google через 7 дней</div>
+			</div>
+			<div class="button">Получить клиентов</div>
 		</div>
-		<div class="container">
-			
+		<div class="wrap">
+			<img src="images/web.jpg" alt="header" class="bg-image">
+			<div class="container">
+				
+			</div>
 		</div>
 	</div>
 		
