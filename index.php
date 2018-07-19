d2374a2b3ca34b4d1fd49f192085f86f53255ae7
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-07-19 20:26:14 +0500

    gg

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/jquery.js"></script>
 	<title>Landing</title>
 </head>
 <body>
+		<div id="container">
+			<div id="progress">
+				
+			</div>
+		</div>
 	<div class="page-wrap">
 		<div class="header">
 			<img src="images/web.jpg" alt="header" class="bg-image">
@@ -26,10 +33,31 @@
 			</div>
 			<div class="button">Получить клиентов</div>
 		</div>
-		<div class="wrap">
-			<img src="images/web.jpg" alt="header" class="bg-image">
+		<div class="wrap" id="wrap_2">
 			<div class="container">
 				
+			</div>
+		</div>
+		<div class="wrap" id="wrap_3">
+			<div class="container">
+				
+			</div>
+		</div>
+		<div class="wrap" id="wrap_4">
+			<div class="container">
+				<div class="order">
+					<form action="">
+						<div class="order-title">
+						Заполните форму 
+						<br>
+						и получи сайт	
+						</div>
+						<input type="text" class="info" placeholder="Имя">
+						<input type="text" class="info" placeholder="Фамилия">
+						<input type="text" class="info" placeholder="Номер">
+						<input type="button" class="info info-btn" value="Оставить заявку" name="button">
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
